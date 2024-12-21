3b0f67f49158d8315399f1d87ae484c56f3954fc
bxdn <33440510+bxdn@users.noreply.github.com>
2024-12-21 11:27:56 -0500

    Now works for multiple

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import numpy as np
 import pyautogui
 from datetime import datetime
 from time import sleep
+import glob
 
-template = cv2.imread('./icon.png', cv2.IMREAD_GRAYSCALE)
+templates = [cv2.imread(file_path, cv2.IMREAD_GRAYSCALE) for file_path in glob.glob('icons/*.png')]
 
 SCALES = np.linspace(0.5, 2.0, 10)
 THRESHOLD = 0.8
@@ -12,12 +13,13 @@ SLEEP_TIME = 10
 
 def found_match():
     scrnsht = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2GRAY)
-    for scale in SCALES:
-        resized_template = cv2.resize(template, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
-        result = cv2.matchTemplate(scrnsht, resized_template, cv2.TM_CCOEFF_NORMED)
-        _, max_val, _, _ = cv2.minMaxLoc(result)
-        if max_val > THRESHOLD:
-            return True
+    for template in templates:
+        for scale in SCALES:
+            resized_template = cv2.resize(template, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+            result = cv2.matchTemplate(scrnsht, resized_template, cv2.TM_CCOEFF_NORMED)
+            _, max_val, _, _ = cv2.minMaxLoc(result)
+            if max_val > THRESHOLD:
+                return True
     return False
 
 while True:
